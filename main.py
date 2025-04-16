858787058ff83f4f31a0fa76f3e726eac434dd69
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-16 11:35:42 +0200

    Add Snake class with drawing functionality

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import pygame
 from pygame.math import Vector2
 
 
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_position = int(block.x * CELL_SIZE)
+            y_position = int(block.y * CELL_SIZE)
+            block_rect = pygame.Rect(
+                x_position,
+                y_position,
+                CELL_SIZE,
+                CELL_SIZE,
+            )
+            pygame.draw.rect(screen, pygame.Color("blue"), block_rect)
+
+
 class Fruit:
     def __init__(self):
         self.x = random.randint(0, CELL_NUMBER - 1)
@@ -30,6 +47,7 @@ screen = pygame.display.set_mode((CELL_NUMBER * CELL_SIZE, CELL_NUMBER * CELL_SI
 clock = pygame.time.Clock()
 
 fruit = Fruit()
+snake = Snake()
 
 while True:
     for event in pygame.event.get():
@@ -39,5 +57,6 @@ while True:
 
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(FPS)
