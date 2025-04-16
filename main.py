cc6a0ff4c89f3dabe632ef506e008426b27720f4
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-17 01:20:33 +0200

    Refactor Snake class to implement growth logic and adjust game update flow

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Snake:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         self.direction = RIGHT
+        self.can_grow = False
 
     def draw_snake(self):
         for block in self.body:
@@ -23,16 +24,23 @@ class Snake:
             pygame.draw.rect(screen, pygame.Color("blue"), block_rect)
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.can_grow is True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.can_grow = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def grow(self):
+        self.can_grow = True
 
 
 class Fruit:
     def __init__(self):
-        self.x = random.randint(0, CELL_NUMBER - 1)
-        self.y = random.randint(0, CELL_NUMBER - 1)
-        self.position = Vector2(self.x, self.y)
+        self.randomise_position()
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(
@@ -42,6 +50,30 @@ class Fruit:
             CELL_SIZE,
         )
         pygame.draw.rect(screen, pygame.Color("red"), fruit_rect)
+
+    def randomise_position(self):
+        self.x = random.randint(0, CELL_NUMBER - 1)
+        self.y = random.randint(0, CELL_NUMBER - 1)
+        self.position = Vector2(self.x, self.y)
+
+
+class SnakeGame:
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+
+    def draw_elements(self):
+        self.snake.draw_snake()
+        self.fruit.draw_fruit()
+
+    def check_collision(self):
+        if self.fruit.position == self.snake.body[0]:
+            self.fruit.randomise_position()
+            self.snake.grow()
 
 
 FPS = 60
@@ -59,8 +91,7 @@ clock = pygame.time.Clock()
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-fruit = Fruit()
-snake = Snake()
+snake_game = SnakeGame()
 
 while True:
     for event in pygame.event.get():
@@ -68,19 +99,18 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            snake.move_snake()
+            snake_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake.direction = UP
+                snake_game.snake.direction = UP
             if event.key == pygame.K_DOWN:
-                snake.direction = DOWN
+                snake_game.snake.direction = DOWN
             if event.key == pygame.K_LEFT:
-                snake.direction = LEFT
+                snake_game.snake.direction = LEFT
             if event.key == pygame.K_RIGHT:
-                snake.direction = RIGHT
+                snake_game.snake.direction = RIGHT
 
     screen.fill((175, 215, 70))
-    fruit.draw_fruit()
-    snake.draw_snake()
+    snake_game.draw_elements()
     pygame.display.update()
     clock.tick(FPS)
